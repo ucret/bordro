fde85972b18fe95864517b41b8ab66729c335017
ucret <109041726+ucret@users.noreply.github.com>
2024-12-06 09:29:03 +0300

    Update gelismis.py

--- gelismis.py
+++ gelismis.py
@@ -58,9 +58,9 @@ ms_yukselme_B_brut=[0]*12
 
  
 
-igv = [2550.32,2550.32,2550.32,2550.32,2550.32,2550.32,3001.06,3400.42,3400.42,3400.42,3400.42,3400.42] #Gelir vergisi istisnası
+igv = [2550.32*1.3,2550.32*1.3,2550.32*1.3,2550.32*1.3,2550.32*1.3,2550.32*1.3,3001.06*1.3,3400.42*1.3,3400.42*1.3,3400.42*1.3,3400.42*1.3,3400.42*1.3] #Gelir vergisi istisnası
 
-tavan = [20002.5 * 7.5 if i < 6 else 20002.5 * 7.5 for i in range(12)] #Emekli sandığı tavanı
+tavan = [20002.5 * 7.5*1.3 if i < 6 else 20002.5 * 7.5*1.3 for i in range(12)] #Emekli sandığı tavanı
 
 devreden1 = [0]*14 #birinci devreden matrah
 
@@ -104,7 +104,7 @@ yemek_GV_istisna=[170]*12
  
 
 def vergi(kum, matrah):  # Vergi hesaplama fonksiyonu (doğru çalışan versiyon)
-    v = [110000, 230000, 870000, 3000000]  # Vergi dilimleri
+    v = [158323, 331039, 1252191, 4317900]  # Vergi dilimleri
     o = [0.15, 0.2, 0.27, 0.35, 0.4]  # Vergi oranları
     kalan_matrah = matrah  # Kalan matrah miktarı
     toplam_vergi = 0  # Toplam vergi
@@ -130,7 +130,7 @@ def vergi(kum, matrah):  # Vergi hesaplama fonksiyonu (doğru çalışan versiyo
     return toplam_vergi
 
 def brut_vergi(kum,net):
-    v = [110000,230000,870000,3000000] # vergi dilimleri
+    v = [158323, 331039, 1252191, 4317900] # vergi dilimleri
     o = [0.15,0.2,0.27,0.35,0.4] #vergi oranları
     damga = 0.00759
     v_brut_bosluk = [0,0,0,0]
@@ -158,7 +158,7 @@ def brut_vergi(kum,net):
     return vergi_brutu
 
 def brut_vergi_sgk(kum,net):
-    v = [110000,230000,870000,3000000] # vergi dilimleri
+    v = [158323, 331039, 1252191, 4317900] # vergi dilimleri
     o = [0.15,0.2,0.27,0.35,0.4] #vergi oranları
     damga = 0.00759
     v_brut_bosluk = [0,0,0,0]
