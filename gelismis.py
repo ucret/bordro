abcf386af2202982059d4c59d4aa331309053597
ucret <109041726+ucret@users.noreply.github.com>
2024-12-09 14:51:47 +0300

    Update gelismis.py

--- gelismis.py
+++ gelismis.py
@@ -699,6 +699,37 @@ with st.expander("Yıllık Ücret Dağılımı", expanded=False):
     # Grafik katmanlama
     donut_chart = base_chart + center_text_label + center_text_value
 
+
+
     # Streamlit üzerinden Donut Chart gösterimi
     st.altair_chart(donut_chart, use_container_width=True)
+
+
+
+
+
+
+from io import StringIO
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+
+
+
+
 
