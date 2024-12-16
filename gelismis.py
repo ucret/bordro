208a9cff3eaa8fb0bf5f60a0848e03ae9fb2265c
ucret <109041726+ucret@users.noreply.github.com>
2024-12-16 16:55:31 +0300

    Update gelismis.py

--- gelismis.py
+++ gelismis.py
@@ -10,7 +10,7 @@ def page2():
     st.title("Second page")
 
 pg = st.navigation([
-    st.Page("page1.py", title="First page", icon="🔥"),
+    st.Page("gelismis.py", title="First page", icon="🔥"),
     st.Page(page2, title="Second page", icon=":material/favorite:"),
 ])
 pg.run()
